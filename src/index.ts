71ac99ddf53039db429520dafd6a55ab67e92c8a
Johannes Wilm <mail@johanneswilm.org>
2026-07-18 20:20:56 +0200

    feat: add state_plugins (tag_input, contributor_input) from @fiduswriter/common
    Move ProseMirror NodeViews for tags_part and contributors_part document parts into
    @fiduswriter/document where they belong with the document schema.
    
    - Copy tag_input/ and contributor_input/ directories with all NodeViews, helpers, plugins
    - Add 11 state-plugin-related types to src/types.ts (PartNodeAttrs, TagInputRefs, etc.)
    - Add subpath exports: ./state_plugins, ./state_plugins/tag_input, ./state_plugins/contributor_input
    - Add dependencies: prosemirror-gapcursor, w3c-keyname
    - Barrel exports from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 export {FW_DOCUMENT_VERSION} from "./schema/index.js"
 export type {
+    AddButtonConstructor,
+    AddButtonLike,
+    AddButtonOptions,
     BibDB,
     BibDBEntries,
     BibDBEntry,
     CommentData,
     Contributor,
+    ContributorsPartOptions,
+    CreateTagEditor,
     CSL,
     CiteprocInstance,
     DocSettings,
@@ -15,16 +20,35 @@ export type {
     FidusDoc,
     FidusMark,
     FidusNode,
+    GetNode,
+    GetPos,
     ImageDB,
     ImageDBEntries,
     ImageDBEntry,
     JSONValue,
     NativeImporterBackend,
+    NextSelection,
     NodeAttrs,
+    PartNodeAttrs,
     SaveCopyE2EE,
+    TagInputRefs,
+    TagsPartOptions,
     Template,
     TemplateFiles,
     Track,
     UploadRevision,
     User
 } from "./types.js"
+
+// State plugins
+export {
+    AddButton,
+    ContributorsPartView
+} from "./state_plugins/contributor_input/index.js"
+export {
+    TagsPartView,
+    createTagEditor,
+    tagInputReferences
+} from "./state_plugins/tag_input/index.js"
+export {nextSelection, submitTag} from "./state_plugins/tag_input/index.js"
+export {placeholderPlugin, pastePlugin} from "./state_plugins/tag_input/index.js"
